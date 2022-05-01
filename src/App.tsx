baa28c658afa929395992318fae26921984e3b12
alestojanovic <alestojanovic@ebay.com>
2022-05-01 07:42:01 +0200

    feat: add timer and skeleton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyTimer from "./MyTimer";
+import Way from "./Way";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 86400);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<MyTimer expiryTimestamp={time} />} />
+          <Route path="/way" element={<Way />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
