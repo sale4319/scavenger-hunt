cf932d88f6d00243c038c80e39edc708e42cb59e
alestojanovic <alestojanovic@ebay.com>
2022-05-01 08:18:51 +0200

    feat: finish component Two and update styling

--- src/Two.tsx
+++ src/Two.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { PrivateRoutes } from "./PrivateRoutes";
+import logo from "./logo.svg";
 
 export const Two = () => {
   const navigate = useNavigate();
   const routeChange = () => {
     navigate(`${PrivateRoutes.PARAM_THREE}`);
   };
-  return <div>Two</div>;
+  return (
+    <div>
+      <button className="two-button" onClick={routeChange}>
+        <img src={logo} className="App-logo" alt="logo" /> <br />
+        Download me and find the gift inside.
+      </button>
+    </div>
+  );
 };
