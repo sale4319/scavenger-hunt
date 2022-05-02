ef6128f3ebc3763326aa7f41a1b3a170d323bd55
alestojanovic <alestojanovic@ebay.com>
2022-05-02 10:58:41 +0200

    feat: setup the question form, converted css to jss

--- src/components/Level/LevelThree.tsx
+++ src/components/Level/LevelThree.tsx
@@ -1,95 +1,100 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Tippy from "@tippyjs/react";
-import { StyleSheet, css } from "aphrodite";
-import { useLock } from "../../util/utils";
-import ValidatedLoginForm from "../../util/LoginMechanism";
-import { PrivateRoutes } from "../../PrivateRoutes";
+// TODO:import React, { useState } from "react";
+// TODO:import { useNavigate } from "react-router-dom";
+// TODO: import Tippy from "@tippyjs/react";
+// TODO:import { StyleSheet, css } from "aphrodite";
+// TODO:import { useLock } from "../../util/utils";
+import ValidatedForm from "../../util/QuestionMechanism";
+// TODO:import { PrivateRoutes } from "../../PrivateRoutes";
 import {
   LevelTwoMessages,
-  PromptMessages,
-  TooltipMessages,
+  // TODO:PromptMessages,
+  // TODO: TooltipMessages,
 } from "../../Messages";
-import "./styles.css";
 
-const styles = StyleSheet.create({
-  questionTooltip: {
-    backgroundColor: "#61dafb",
-    color: "#fff",
-    textAlign: "center",
-    padding: "8px",
-    borderRadius: "6px",
+// TODO: Styling
+// const styles = StyleSheet.create({
+//   questionTooltip: {
+//     backgroundColor: "#61dafb",
+//     color: "#fff",
+//     textAlign: "center",
+//     padding: "8px",
+//     borderRadius: "6px",
 
-    "::after": {
-      content: '" "',
-      position: "absolute",
-      top: "100%",
-      left: "50%",
-      marginLeft: " -5px",
-      borderWidth: "5px",
-      borderStyle: "solid",
-      borderColor: "#61dafb transparent transparent transparent",
-    },
-  },
-  redButton: {
-    background: "linear-gradient(#eee, #333)",
-    borderStyle: "solid",
-    borderRadius: "9px",
-    cursor: "pointer",
-    padding: "8px",
-    color: "white",
-    ":hover": {
-      background: "linear-gradient(#ff0000, #333)",
-      borderColor: "#ff0000",
-    },
+//     "::after": {
+//       content: '" "',
+//       position: "absolute",
+//       top: "100%",
+//       left: "50%",
+//       marginLeft: " -5px",
+//       borderWidth: "5px",
+//       borderStyle: "solid",
+//       borderColor: "#61dafb transparent transparent transparent",
+//     },
+//   },
+//   redButton: {
+//     background: "linear-gradient(#eee, #333)",
+//     borderStyle: "solid",
+//     borderRadius: "9px",
+//     cursor: "pointer",
+//     padding: "8px",
+//     color: "white",
+//     ":hover": {
+//       background: "linear-gradient(#ff0000, #333)",
+//       borderColor: "#ff0000",
+//     },
 
-    "::selection": { background: "transparent" },
-  },
+//     "::selection": { background: "transparent" },
+//   },
 
-  greenButton: {
-    background: "linear-gradient(#98fe00, #333)",
-    borderStyle: "solid",
-    borderColor: "#adff2f",
-    borderRadius: "9px",
-    cursor: "pointer",
-    padding: "8px",
-    color: "white",
-    ":hover": {
-      background: "linear-gradient(#333, #98fe00)",
-      borderColor: "#cbf094",
-    },
+//   greenButton: {
+//     background: "linear-gradient(#98fe00, #333)",
+//     borderStyle: "solid",
+//     borderColor: "#adff2f",
+//     borderRadius: "9px",
+//     cursor: "pointer",
+//     padding: "8px",
+//     color: "white",
+//     ":hover": {
+//       background: "linear-gradient(#333, #98fe00)",
+//       borderColor: "#cbf094",
+//     },
 
-    "::selection": { background: "transparent" },
-  },
-});
+//     "::selection": { background: "transparent" },
+//   },
+// });
 
 export const LevelThree = () => {
-  const navigate = useNavigate();
-  const [unLockNavigation, setUnlockNavigation] = useState(true);
+  // TODO: Navigation
+  // const navigate = useNavigate();
+  // TODO:Navigation unlock
+  //const [unLockNavigation, setUnlockNavigation] = useState(true);
 
-  const routeChange = () => {
-    navigate(`${PrivateRoutes.PARAM_END_CLASSIC}`);
-  };
+  // TODO:Navigation
+  // const routeChange = () => {
+  //   navigate(`${PrivateRoutes.PARAM_END_CLASSIC}`);
+  // };
 
-  useLock(`${PromptMessages.DEFAULT}`, unLockNavigation);
+  // TODO:Navigation
+  //useLock(`${PromptMessages.DEFAULT}`, unLockNavigation);
 
-  const handleUnlockNavigation = () => {
-    setUnlockNavigation(false);
-  };
+  // TODO: Navigation unlock
+  // const handleUnlockNavigation = () => {
+  //   setUnlockNavigation(false);
+  // };
 
   return (
     <div>
-      <button
+      <h3>{LevelTwoMessages.HINT}</h3>
+      {/* TODO: <button
         className={css(
           unLockNavigation ? styles.redButton : styles.greenButton
         )}
         onClick={routeChange}
       >
         {LevelTwoMessages.CONTINUE}
-      </button>
-      <h3>{LevelTwoMessages.HINT}</h3>
+      </button> */}
       <div>
-        <ValidatedLoginForm />
+        <ValidatedForm />
       </div>
     </div>
   );
