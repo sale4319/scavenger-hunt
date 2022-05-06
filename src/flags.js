b0872366810876c02833e06fe61003c213a06189
alestojanovic <alestojanovic@ebay.com>
2022-05-06 19:05:30 +0200

    refactor: put light mode button behind feature flag

--- src/flags.js
+++ src/flags.js
@@ -1,3 +1,3 @@
 export const featFlags = { test: true };
 
-export const modes = { promptMode: false };
+export const modes = { promptMode: false, lightMode: false };
--- src/stories/headers/AppBar/AppBar.tsx
+++ src/stories/headers/AppBar/AppBar.tsx
@@ -1,6 +1,7 @@
 import { GithubButton } from "../../buttons/";
 import reactHeart from "../../assets/reactHeart.svg";
 import { EndClassicMessages } from "../../../Messages";
+import { modes } from "../../../flags";
 import "./AppBar.css";
 
 type User = {
@@ -33,8 +34,13 @@ export const AppBar = ({
       </div>
       <div className="separator" />
       <div>
-        <img src={reactHeart} alt="gift-logo" />
-        <h1>Light Mode</h1>
+        {/* TODO: enable when I make light mode */}
+        {modes.lightMode && (
+          <>
+            <img src={reactHeart} alt="gift-logo" />
+            <h1>Light Mode</h1>
+          </>
+        )}
       </div>
     </div>
   </header>
