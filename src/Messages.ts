c14fd67971b8a153c6203f8970769b6dcf853100
alestojanovic <aleksandar.stojanovic@productdock.com>
2022-10-07 11:17:27 +0200

    fix: clean up

--- src/Messages.ts
+++ src/Messages.ts
@@ -1,7 +1,6 @@
 export const StartTimerMessages = {
   TITLE: "Welcome, here are your pointers:",
-  INSTRUCTION:
-    "In each level you will have to solve a puzzle to enable the red button, each level is followed up by a quiz, all answers must be correct to continue. You have an option to skip any level or quiz by unlocking red button, but skipping will never take you to the end level.",
+  INSTRUCTION:"In each level you will have to solve a puzzle to enable the red button, each level is followed up by a quiz, all answers must be correct to continue. You have an option to skip any level or quiz by unlocking red button, but skipping will never take you to the end level.",
   HINT: "This button seems to be broken... can we fix it",
   BUTTON: "You have to start somewhere",
 };
